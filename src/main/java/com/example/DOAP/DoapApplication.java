ba256d3a73412d91c6656b9fe2de21a7536a040f
Tilak-1604 <vaghasiyatilak16@gmail.com>
2025-12-25 00:46:53 +0530

    project structure and user entity

--- src/main/java/com/example/DOAP/DoapApplication.java
+++ src/main/java/com/example/DOAP/DoapApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DoapApplication {
 
 	public static void main(String[] args) {
-        System.out.println("tilak");
+        System.out.println("tixlak");
 
 		SpringApplication.run(DoapApplication.class, args);
 	}
